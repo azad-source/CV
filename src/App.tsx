c64522dc3fdde6ed1709852f037ff30f4e02d960
Azad <azad.63.mamedov@gmail.com>
2022-01-06 16:53:07 +0400

    Рефакторинг путей

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'normalize.css';
-import './App.scss';
-import mainBg from './assets/images/main-bg.png';
+import './styles/app.scss';
+import mainBg from './images/main-bg.png';
 import { Header } from './components/Header/Header';
 
 function App() {
