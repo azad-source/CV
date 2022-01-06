d79efc8e905e8828aa6400189020bb2fa3eb8f3e
Azad <azad.63.mamedov@gmail.com>
2022-01-06 15:43:59 +0400

    Доп настройки eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,22 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    "prettier"
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+    extends: ['plugin:react/recommended', 'airbnb', 'airbnb-typescript', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'import/prefer-default-export': 'off',
+        'react/function-component-definition': 'off',
+        'arrow-body-style': 'off',
+    },
 };
