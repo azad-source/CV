6ddfed2a7d9948bdc68436c744fff09f09be131b
Azad <azad.63.mamedov@gmail.com>
2022-01-08 01:06:18 +0400

    Education and  experiences from data

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -2,51 +2,36 @@ import * as React from 'react';
 import cn from 'classnames';
 import { Page } from 'components/layout/Page/Page';
 import { CircleIcon } from 'components/layout/CircleIcon/CircleIcon';
+import { data } from 'data';
 import styles from './Education.module.scss';
 
 const Circle = () => <CircleIcon className={styles.circle} />;
+
+const { education } = data;
 
 export const Education = () => {
     return (
         <Page id="education">
             <h2>Education</h2>
-            <div className={styles['experience-wrap']}>
-                <div className={cn(styles['experience-block'], styles['experience-block-left'])}>
-                    <h3>Bachelor Of Engineering (aircraft engines and power plants)</h3>
-                    <div className={styles['experience-block-text']}>
-                        <div>
-                            <strong>Samara State Aerospace University </strong>
-                            <em>(september 2008 - july 2011)</em>
-                            <p />
+            <div className={styles.root}>
+                {education.map(({ establishment, description, degree, date }, index) => (
+                    <div
+                        className={cn(
+                            styles.block,
+                            styles[`block_${(index + 1) % 2 === 0 ? 'right' : 'left'}`],
+                        )}
+                    >
+                        <h3>{degree}</h3>
+                        <div className={styles.block__text}>
+                            <div>
+                                <strong>{establishment} </strong>
+                                <em>({date})</em>
+                                <p>{description}</p>
+                            </div>
                         </div>
+                        <Circle />
                     </div>
-                    <Circle />
-                </div>
-                <div className={cn(styles['experience-block'], styles['experience-block-right'])}>
-                    <h3>Master Of Engineering (Aircraft Engine)</h3>
-                    <div className={styles['experience-block-text']}>
-                        <div>
-                            <strong>Samara State Aerospace University </strong>
-                            <em>(september 2011 - july 2014)</em>
-                            <p />
-                        </div>
-                    </div>
-                    <Circle />
-                </div>
-                <div className={cn(styles['experience-block'], styles['experience-block-left'])}>
-                    <h3>Frontend developer</h3>
-                    <div className={styles['experience-block-text']}>
-                        <div>
-                            <strong>EPAM courses </strong>
-                            <em>(september 2020 - december 2020)</em>
-                            <p>
-                                JS Core, Event Loop, JavaScript Asynchronous, features of es6/7/8,
-                                ReactJS
-                            </p>
-                        </div>
-                    </div>
-                    <Circle />
-                </div>
+                ))}
             </div>
         </Page>
     );
--- src/data.ts
+++ src/data.ts
@@ -57,83 +57,83 @@ export const data = {
         },
     },
     about: 'I have more than 3 years of experience in web development and I really like to work with customers and provide high-quality service and quick responses. I like to discover new technologies, search out for effective decisions, and grow as a professional. Working with a variety of technologies and tools helps me to use proven best practices for existing challenges, predict application behavior and prevent bugs and incidents. Contact me if you want to work with a reliable and experienced developer. Lets get the job done!',
-    education: {
-        '1': {
+    education: [
+        {
             degree: 'Bachelor Of Engineering (aircraft engines and power plants)',
             establishment: 'Samara State Aerospace University',
             date: 'september 2008 - july 2011',
             description: '',
         },
-        '2': {
+        {
             degree: 'Master Of Engineering (Aircraft Engine)',
             establishment: 'Samara State Aerospace University',
             date: 'september 2011 - july 2014',
             description: '',
         },
-        '3': {
+        {
             degree: 'Frontend developer',
             establishment: 'EPAM courses',
             date: 'september 2020 - december 2020',
             description:
                 'JS Core, Event Loop, JavaScript Asynchronous, features of es6/7/8, ReactJS',
         },
-    },
-    experience: {
-        '1': {
+    ],
+    experience: [
+        {
             employe: 'Mechanical Engineer',
             company: 'Safran SMARTEC - Samara',
             date: 'feb  2014 - dec 2018',
             description:
                 'Structural linear and nonlinear analysis of the intermediate engine mount for LEAP-1, starting from the creation of finite element model, application of boundary conditions and definition materials property, and ending with the launch of the calculation and post processing of results. Life time calculation and modal analysis to determine natural frequencies. Software I used: ICEM (to create the mesh) Patran (to create meshing and boundary conditions) Ansys Workbench (for geometry preparation, to create mesh, boundary condition and to start the calculation) Samcef (to create condition to the limits and launch of the linear / non-linear computation (asef / mechanics)) Abaqus (to create boundary condition and to start the computation linear / nonlinear) At the end, an analysis of the results, and a report.',
         },
-        '2': {
+        {
             employe: 'Web Developer',
             company: 'Plusmedia - Samara',
             date: 'jan 2019 – feb 2021',
             description:
                 'Development, support and seo promotion of websites of any complexity on cms modx, bitrix, simpla, wordperss, joomla. Working with API of modx and bitrix. Preparing of environment for the website release, installation of modules (nginx, apache, php, sql etc) and their configuration. Request processing speed optimization on the website. Languages used in development: html5, css3, javascript, jquery, php, mysql. At the moment I am studying the development of websites on React.',
         },
-        '3': {
-            employe: 'frontend developer',
+        {
+            employe: 'Frontend developer',
             company: 'SKB Kontur',
             date: 'mar 2021 – to Present',
             description:
                 'Develop the SPA in React JS framework, working with Redux architecture using complex Object-Oriented concepts in improving the performance of the websites.',
         },
-    },
+    ],
     portfolios: {
-        'Java Script ES5/ES6/ES7': {
-            '1': {
+        'Java Script ES5/ES6/ES7': [
+            {
                 name: 'Webdev',
                 link: './portfolios/webdev/',
                 image: 'webdev.png',
             },
-            '2': {
+            {
                 name: 'Calculator',
                 link: './portfolios/calculator/',
                 image: 'calculator.png',
             },
-            '3': {
+            {
                 name: 'Momentum',
                 link: './portfolios/momentum/',
                 image: 'momentum.png',
             },
-            '4': {
+            {
                 name: 'Virtual keyboard',
                 link: './portfolios/virtual-keyboard/',
                 image: 'virtual-keyboard.png',
             },
-            '5': {
+            {
                 name: 'Pure water',
                 link: './portfolios/pure-water/',
                 image: 'pure-water.png',
             },
-            '6': {
+            {
                 name: 'Timeline dashboard',
                 link: './portfolios/timeline-dashboard/',
                 image: 'timeline-dashboard.png',
             },
-        },
+        ],
     },
     skills: {
         'CMS / Frameworks / library': {
@@ -161,16 +161,16 @@ export const data = {
             MongoDB: '20%',
         },
     },
-    language: {
-        '1': {
+    language: [
+        {
             language: 'english',
             level: 'A2',
             description: 'intermediate, conversational, competent',
         },
-        '2': {
+        {
             language: 'french',
             level: 'B1',
             description: 'had experience of correspondence and communication with native speakers',
         },
-    },
+    ],
 };
